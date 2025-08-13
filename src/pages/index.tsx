952d498e03546efb7b72f4211ef9eeed6f5470f3
William Huebner <wilhueb@gmail.com>
2025-08-13 06:13:49 -0400

    better styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,23 +4,20 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { api } from "@/utils/api";
 
-function Header() {
+function Navbar() {
   const { data: sessionData } = useSession();
 
   return (
-    <div className="flex items-center justify-between gap-4 bg-gray-800 px-4 py-1 text-white">
+    <nav className="bg-card text-card-foreground m-2 flex items-center justify-between gap-4 rounded-md border-1 px-3 py-1">
       <Link href="/" className="text-2xl">
         CFB Picks
       </Link>
       <div>
-        <Button
-          className="bg-gray-300 text-gray-800"
-          onClick={sessionData ? () => void signOut() : () => void signIn()}
-        >
+        <Button variant="ghost" onClick={sessionData ? () => void signOut() : () => void signIn()}>
           {sessionData ? "Sign out" : "Sign in"}
         </Button>
       </div>
-    </div>
+    </nav>
   );
 }
 
@@ -29,14 +26,14 @@ export default function Home() {
 
   return (
     <>
-      <Header />
+      <Navbar />
       <main className="min-h-screen w-screen">
         <div className="flex w-full flex-col items-center gap-4 p-4">
           <h2 className="text-xl">Games for Week 1 of 2025</h2>
           {games.isLoading ? (
             <p>Loading...</p>
           ) : games.isError ? (
-            <p className="text-red-500">Error: {games.error.message}</p>
+            <p className="text-destructive">Error: {games.error.message}</p>
           ) : (
             <ul className="list-disc">
               {games.data

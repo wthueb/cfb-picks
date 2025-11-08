508f9bc4ce4c47cd55ede4b5a29910f423dc314d
William Huebner <wilhueb@gmail.com>
2025-11-08 13:15:06 -0500

    add error catching to redis client

--- packages/cfbd/src/cache.ts
+++ packages/cfbd/src/cache.ts
@@ -4,6 +4,15 @@ import { env } from "./env.js";
 
 export const client = createClient({
   url: env.REDIS_URL,
+  socket: {
+    reconnectStrategy: () => 1000,
+  },
 });
+
+client.on("error", (err) => console.error("Redis error:", err));
+client.on("connect", () => console.log("Redis connecting..."));
+client.on("ready", () => console.log("Redis reporting ready"));
+client.on("end", () => console.warn("Redis connection closed"));
+client.on("reconnecting", () => console.log("Redis reconnecting..."));
 
 await client.connect();
